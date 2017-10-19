8f2621092edc0404c65de381c758d377deaf658f
gusna <gusna@GUSNAIKHSAN>
2017-10-20 00:58:55 +0700

    edit form unggah

--- MangCook/Properties/Resources.Designer.cs
+++ MangCook/Properties/Resources.Designer.cs
@@ -293,6 +293,16 @@ namespace MangCook.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap daun {
+            get {
+                object obj = ResourceManager.GetObject("daun", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap drinkRed {
             get {
                 object obj = ResourceManager.GetObject("drinkRed", resourceCulture);
